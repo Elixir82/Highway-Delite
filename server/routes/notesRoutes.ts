5d3349fde00d0cf680cf1481b7fd1d11c9e87443
Elixir82 <timepass17281@gmail.com>
2025-09-01 18:08:09 +0530

    render commit 3

--- server/routes/notesRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router= express.Router();
-import { addNotes, getNotes, deleteNode } from '../src/controllers/notesController';
-router.get('/notes',getNotes);
-router.post('/addNote',addNotes);
-router.post('/deleteNote/:id',deleteNode);
-
-export default router;
